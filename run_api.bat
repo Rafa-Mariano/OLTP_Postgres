62b896d7429faf971318f65edcf7fd85030fc187
RAFAEL MARIANO <40532001+Rafa-Mariano@users.noreply.github.com>
2026-04-25 22:25:31 -0300

    Update script to activate virtual environment

--- run_api.bat
+++ run_api.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Script para rodar a API no Windows
-cd /d "C:\Users\Ana Clara\Downloads\Projeto_OLTP\OLTP_Postgres"
+cd /d "C:\Users\..."
 
-REM Ativar venv_novo
-call venv_novo\Scripts\activate.bat
+REM Ativar venv
+call venv\Scripts\activate.bat
 
 REM Entrar na pasta api
 cd api
